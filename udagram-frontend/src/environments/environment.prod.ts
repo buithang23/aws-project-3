2c7968d142cc165f808db676e667d39bde0d8aff
buithang <buivietthang2399@gmail.com>
2024-10-06 12:42:57 +0700

    update host

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a26bf679e59c54dc886a877ff1832f06-1287305634.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
